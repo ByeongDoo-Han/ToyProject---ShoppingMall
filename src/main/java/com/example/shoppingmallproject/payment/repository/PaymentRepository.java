76ec94278c7f3744cc13395cb84e431a21cd778c
byeongdoohan <quden04@gmail.com>
2023-06-29 23:45:31 +0900

    refactor:PaymentRepository BigInteger -> Long

--- src/main/java/com/example/shoppingmallproject/payment/repository/PaymentRepository.java
+++ src/main/java/com/example/shoppingmallproject/payment/repository/PaymentRepository.java
@@ -4,8 +4,7 @@ import com.example.shoppingmallproject.payment.entity.Payment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.math.BigInteger;
 
 @Repository
-public interface PaymentRepository extends JpaRepository<Payment, BigInteger> {
+public interface PaymentRepository extends JpaRepository<Payment, Long> {
 }
