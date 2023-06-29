16d28ffdd4b149b5cd76692497c913069e9acc3c
byeongdoohan <quden04@gmail.com>
2023-06-29 23:45:01 +0900

    refactor:order repository BigIntger->Long

--- src/main/java/com/example/shoppingmallproject/order/repository/OrderRepository.java
+++ src/main/java/com/example/shoppingmallproject/order/repository/OrderRepository.java
@@ -4,8 +4,7 @@ import com.example.shoppingmallproject.order.entity.Orders;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.math.BigInteger;
 
 @Repository
-public interface OrderRepository extends JpaRepository<Orders, BigInteger> {
+public interface OrderRepository extends JpaRepository<Orders, Long> {
 }
