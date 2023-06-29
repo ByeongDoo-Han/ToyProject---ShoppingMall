51ac56716a340a7d156eb878ccf3125a99bd2e53
byeongdoohan <quden04@gmail.com>
2023-06-29 23:43:48 +0900

    refactor:user, address 관계설정

--- src/main/java/com/example/shoppingmallproject/address/entity/Address.java
+++ src/main/java/com/example/shoppingmallproject/address/entity/Address.java
@@ -1,23 +1,30 @@
 package com.example.shoppingmallproject.address.entity;
 
+import com.example.shoppingmallproject.user.entity.User;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-
-import java.math.BigInteger;
+import lombok.extern.slf4j.Slf4j;
 
 @Entity
 @Getter
 @RequiredArgsConstructor
 @AllArgsConstructor
+@Table(name = "ADDRESS")
+@Builder
+@Slf4j
 public class Address {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private BigInteger id;
+    @Column(name = "ADDRESS_ID")
+    private Long id;
     @Column
     private String userAddress;
     @Column
-    private BigInteger userId;
+    private Long userId;
+    @OneToOne(mappedBy = "address", fetch = FetchType.LAZY)
+    private User users;
 }
