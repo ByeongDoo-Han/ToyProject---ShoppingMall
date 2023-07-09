1ab266097c2c330edae085b67a5659acae695413
DK <laancer4@gmail.com>
2023-07-10 02:09:22 +0900

    Refactor : User 엔티티 테이블을, "USERS" 라는 이름으로 변경

--- src/main/java/com/example/shoppingmallproject/user/entity/User.java
+++ src/main/java/com/example/shoppingmallproject/user/entity/User.java
@@ -8,7 +8,7 @@ import lombok.extern.slf4j.Slf4j;
 import java.util.LinkedHashSet;
 import java.util.Set;
 
-@Entity
+@Entity(name = "USERS")
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class User extends TimeStamped {
