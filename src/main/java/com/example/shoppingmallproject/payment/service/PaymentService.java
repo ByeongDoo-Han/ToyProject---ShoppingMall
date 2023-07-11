8638cccc92b741dd9a47a278340391a37cd74727
byeongdoohan <quden04@gmail.com>
2023-07-11 21:02:23 +0900

    Feat:payments 조회 method 구현

--- src/main/java/com/example/shoppingmallproject/payment/service/PaymentService.java
+++ src/main/java/com/example/shoppingmallproject/payment/service/PaymentService.java
@@ -1,10 +1,17 @@
 package com.example.shoppingmallproject.payment.service;
 
 import com.example.shoppingmallproject.payment.dto.PaymentRequestDto;
+import com.example.shoppingmallproject.payment.dto.PaymentsResultDto;
+import com.example.shoppingmallproject.user.entity.User;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 public interface PaymentService {
 
     @Transactional
     void pay(PaymentRequestDto paymentRequestDto);
+
+    @Transactional
+    List<PaymentsResultDto> getPayments(User user);
 }
