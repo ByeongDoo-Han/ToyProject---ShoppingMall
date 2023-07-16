10c50565d0634dae4237d428b4fc468a4d365229
byeongdoohan <quden04@gmail.com>
2023-07-16 17:07:01 +0900

    Refactor: 충돌 수정

--- src/test/java/com/example/shoppingmallproject/user/entity/UsersTest.java
+++ src/test/java/com/example/shoppingmallproject/user/entity/UsersTest.java
@@ -14,34 +14,6 @@ public class UsersTest {
     private AddressRepository addressRepository;
     @Test
     public void oneToOneTest(){
-<<<<<<< HEAD
-        String email = "quden04@gmail.com";
-        Address address= Address.builder()
-            .userAddress("광명시")
-            .build();
-        User user1 = User.builder()
-            .email(email)
-            .password("asdf")
-            .phone("010-1234-1234")
-            .username("병두")
-            .build();
-        usersRepository.saveAndFlush(user1);
-        addressRepository.saveAndFlush(address);
-        System.out.println("user검색==============");
-        User user2 = usersRepository.findByEmail(user1.getEmail()).orElseThrow(
-            () -> new NullPointerException()
-        );
-        System.out.println("address검색===========");
-        Address address2 = addressRepository.findById(address.getId()).orElseThrow(
-            () -> new NullPointerException()
-        );
-        System.out.println("user검색2==============");
-        User resultUser = usersRepository.findById(1L).get();
-        System.out.println("address검색2===========");
-        System.out.println("add검색3========");
-//        Address resultAddress2 = usersRepository.findAddressUsingJoin(resultUser);
-=======
 
->>>>>>> a4e967323233060d176cd776fcb13de0fae1f8c7
     }
 }
