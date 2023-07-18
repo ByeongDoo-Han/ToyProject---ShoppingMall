230c14f1610f8c129ba4e40fc8731701c106587e
dangddoong <dangddoong@gmail.com>
2023-07-18 14:02:45 +0900

    Feat: Jwt Token -> create, resolve 관련 로직 추가 / Filter 주석 해제 및 token 관련 로직 수정

--- src/main/java/com/example/shoppingmallproject/common/security/filter/AdminAuthFilter.java
+++ src/main/java/com/example/shoppingmallproject/common/security/filter/AdminAuthFilter.java
@@ -1,12 +1,15 @@
 package com.example.shoppingmallproject.common.security.filter;
 
 import com.example.shoppingmallproject.common.security.jwt.JwtUtil;
+import com.example.shoppingmallproject.common.security.userDetails.service.UserDetailsServiceType;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 @RequiredArgsConstructor
@@ -17,18 +20,18 @@ public class AdminAuthFilter extends OncePerRequestFilter {
   @Override
   protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
       FilterChain filterChain) throws ServletException, IOException {
-//    try {
-//      String adminToken = jwtUtil.resolveAdminToken(request);
-//      if (adminToken != null) {
-//        String loginId = jwtUtil.getLoginIdFromTokenIfValid(adminToken);
-//        Authentication authentication = jwtUtil.createAuthentication(loginId,
-//            UserDetailsServiceType.ADMIN);
-//        SecurityContextHolder.getContext().setAuthentication(authentication);
-//      }
-//    } catch (Exception e) {
-//      response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-//      return;
-//    }
+    try {
+      String adminToken = jwtUtil.resolveToken(request,JwtUtil.ADMIN_HEADER);
+      if (adminToken != null) {
+        String loginId = jwtUtil.getLoginIdFromToken(adminToken);
+        Authentication authentication = jwtUtil.createAuthentication(loginId,
+            UserDetailsServiceType.ADMIN);
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+      }
+    } catch (Exception e) {
+      response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+      return;
+    }
     filterChain.doFilter(request, response);
   }
 }
