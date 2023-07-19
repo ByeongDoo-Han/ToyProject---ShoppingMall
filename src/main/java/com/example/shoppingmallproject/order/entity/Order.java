c2e597e5d20567a13448fa783d901e940b62885f
dangddoong <116885047+dangddoong@users.noreply.github.com>
2023-07-19 17:51:15 +0900

    Feat: ERD 변경에 따른 Order Entity 수정

--- src/main/java/com/example/shoppingmallproject/order/entity/Order.java
+++ src/main/java/com/example/shoppingmallproject/order/entity/Order.java
@@ -1,7 +1,6 @@
 package com.example.shoppingmallproject.order.entity;
 
 import com.example.shoppingmallproject.orderPayDelivery.entity.OrderPayDelivery;
-import com.example.shoppingmallproject.payment.entity.Payment;
 import com.example.shoppingmallproject.share.TimeStamped;
 import com.example.shoppingmallproject.user.entity.User;
 import jakarta.persistence.*;
@@ -13,20 +12,17 @@ import java.util.List;
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Orders extends TimeStamped {
+public class Order extends TimeStamped {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @ManyToOne(fetch = FetchType.LAZY)
     private User user;
-    @OneToMany(fetch = FetchType.LAZY)
-    private List<OrderPayDelivery> orderPayDelivery;
-    private Long orderPrice;
+    private Long totalPrice;
 
     @Builder
-    public Orders(User user, List<OrderPayDelivery> orderPayDelivery, Long orderPrice) {
+    public Order(User user, Long totalPrice) {
         this.user = user;
-        this.orderPayDelivery = orderPayDelivery;
-        this.orderPrice = orderPrice;
+        this.totalPrice = totalPrice;
     }
 }
